30eeacb66e24ea1dec7c5bfea2d640400639ae94
Naimur Abir <abir8619@student.acitech.org>
2025-03-14 12:55:24 -0400

    small changes

--- quick_Sort/src/Main.java
+++ quick_Sort/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
             int rand_int = rand.nextInt(99) + 1;
             intArray[index] = rand_int;
         }
+        intArray[4] = 1;
         for(int num:intArray){
             if(num != intArray[intArray.length - 1])
                 System.out.printf("%s, ",num);
@@ -15,7 +16,9 @@ public class Main {
                 System.out.print(num);
             }
         }
-        System.out.println("\n" + partition(intArray, 0, intArray.length - 1));
+        System.out.println();
+
+        quickSort(intArray, 0, intArray.length - 1);
         for(int num:intArray) {
             if (num != intArray[intArray.length - 1])
                 System.out.printf("%s, ", num);
@@ -23,6 +26,8 @@ public class Main {
                 System.out.print(num);
             }
         }
+        System.out.println();
+
     }
 
     public static int partition(int[] arr, int low, int high){
@@ -32,10 +37,67 @@ public class Main {
     }
 
     public static void swap(int[] arr, int i, int j){
-        if(arr[i] < arr[j]){
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        for(int index_low = 0; index_low < i; index_low++){
+            printArray(arr);
+            System.out.println(arr[index_low] + " " + arr[j]);
+            if(arr[index_low] > arr[j]){
+                int index_high = j - 1;
+                for(int x = 0; index_high > 0; index_high--) {
+                    System.out.println(arr[index_low] + " " + arr[j] + " " + arr[index_high]);
+                    if (arr[j] > arr[index_high]) {
+                        temp = arr[index_low];
+                        arr[index_low] = arr[index_high];
+                        arr[index_high] = temp;
+                        printArray(arr);
+                        index_high = 0;
+                    }
+                }
+                if(index_high == 0){
+                    int[] intArray = new int[10];
+                    int indexTwo = 0;
+                    for(int index = 0; index < intArray.length - 2; index ++){
+                        if(index_low == indexTwo){
+                            indexTwo++;
+                        }
+                        intArray[index] = arr[indexTwo];
+                        indexTwo++;
+                    }
+                    intArray[intArray.length - 2] = arr[index_low];
+                    intArray[intArray.length - 1] = arr[intArray.length - 1];
+                    arr = intArray;
+                }
+            }
+            printArray(arr);
         }
+        int index = i;
+        temp = arr[index];
+        while(temp == arr[index]){
+            if(arr[index] > arr[j]){
+                arr[index] = arr[j];
+                arr[j] = temp;
+                temp = -1;
+            }
+            else{
+                index++;
+            }
+        }
+
+    }
+
+    static void quickSort(int[] arr, int low, int high){
+        System.out.println((high - low));
+        if((high - low) > 0){
+            int middle = partition(arr, low, high);
+        }
+    }
+
+    public static void printArray(int[] arr){
+        for(int num:arr) {
+            System.out.printf("%s, ", num);
+        }
+        System.out.println();
     }
 }
